15897cce706fe1c22fa6f97f57eba9c8062aed3f
Spencer Parkin <stparkin@mmm.com>
2017-01-27 12:50:33 -0700

    initial idea for frag shader

--- Data/Shaders/face.f.glsl
+++ Data/Shaders/face.f.glsl
@@ -1,9 +1,34 @@
 #version 110
 
-uniform vec3 color;
-uniform float alpha;
+uniform vec3 triangleVertex[3];
+uniform float borderMask[3];
+uniform float maxSubTriangleArea;
+uniform vec3 borderColor;
+
+varying vec4 trianglePoint;
 
 void main()
 {
-	gl_FragColor = vec4( color, alpha );
+	float smallestSubTriangleArea = 1000.0;
+	
+	for( int i = 0; i < 3; i++ )
+	{
+		vec3 vertexA = triangleVertex[i];
+		vec3 vertexB = triangleVertex[ mod( i + 1, 3 ) ];
+		
+		vec3 edgeA = vertexA - trianglePoint;
+		vec3 edgeB = vertexB - trianglePoint;
+		
+		vec3 cross = cross( edgeA, edgeB );
+		subTriangleArea = length( cross ) / 2.0;
+		subTriangleArea += borderMask[i] * maxSubTriangleArea;
+		
+		smallestSubTriangleArea = min( subTriangleArea, smallestSubTriangleArea );
+	}
+	
+	float areaRatio = min( smallestSubTriangleArea / maxSubTriangleArea, 1.0 );
+	
+	vec3 color = mix( borderColor, gl_Color, areaRatio );
+	
+	gl_FragColor = vec4( color, 1.0 );
 }
--- Data/Shaders/face.v.glsl
+++ Data/Shaders/face.v.glsl
@@ -1,6 +1,10 @@
 #version 110
 
+varying vec4 trianglePoint;
+
 void main()
 {
+	trianglePoint = gl_Vertex;
+	
 	gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
 }
