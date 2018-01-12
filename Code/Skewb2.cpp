0c4f3dd63f7bd37f7b338e4b16caed9add9ad0fe
Spencer Parkin <stparkin@mmm.com>
2018-01-12 12:51:35 -0700

    add skewb2 middle slice moves

--- Code/Skewb2.cpp
+++ Code/Skewb2.cpp
@@ -18,8 +18,6 @@ Skewb2::Skewb2( void )
 {
 	Clear();
 
-	SetupStandardDynamicCornerTurningBoxLabels();
-
 	MakeBox( 10.0, 10.0, 10.0 );
 
 	_3DMath::Vector normal[8];
@@ -36,6 +34,8 @@ Skewb2::Skewb2( void )
 		normal[i].Normalize();
 	}
 
+	char label = 'A';
+
 	for( int i = 0; i < 8; i++ )
 	{
 		_3DMath::Vector center;
@@ -45,10 +45,19 @@ Skewb2::Skewb2( void )
 		cutShape->surface = new _3DMath::PlaneSurface( _3DMath::Plane( center, normal[i] ) );
 		cutShape->rotationAngleForSingleTurn = 2.0 * M_PI / 3.0;
 		cutShape->axisOfRotation.normal = normal[i];
+		cutShape->label = label++;
 		cutShapeList.push_back( cutShape );
-	}
 
-	// TODO: Make double-surface cut shapes here.
+		DoubleSurfaceCutShape* doubleCutShape = new DoubleSurfaceCutShape();
+		doubleCutShape->surface = new _3DMath::PlaneSurface( _3DMath::Plane( center, normal[i] ) );
+		doubleCutShape->additionalSurface = new _3DMath::PlaneSurface( _3DMath::Plane( center * -1.0, normal[i] * -1.0 ) );
+		doubleCutShape->rotationAngleForSingleTurn = 2.0 * M_PI / 3.0;
+		doubleCutShape->axisOfRotation.normal = normal[i];
+		doubleCutShape->label = label++;
+		doubleCutShape->vectorLength = 12.0;
+		doubleCutShape->captureSide = _3DMath::Surface::INSIDE;
+		cutShapeList.push_back( doubleCutShape );
+	}
 }
 
 // Skewb2.cpp
